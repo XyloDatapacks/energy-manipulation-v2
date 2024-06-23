94b72ab75636d119625449341d880f918726a737
XyloDatapacks <xylo.datapacks@gmail.com>
2024-06-23 17:32:55 +0200

    fixed prev commit. but this will not work in the long run. i should register subnodes after registering all nodes somehow
    (reason is for example that shape will have an instruction provider which is declared after the shape nodes, since they are generated from an instruction themselves)

--- src/main/java/com/xylo_datapacks/energy_manipulation/item/custom/spell_book/node/Nodes.java
+++ src/main/java/com/xylo_datapacks/energy_manipulation/item/custom/spell_book/node/Nodes.java
@@ -22,31 +22,18 @@ import java.util.Map;
 
 public class Nodes {
     public static Map<Identifier, NodeData> NODES = new HashMap<>();
-   
+
+    public static final NodeData EFFECT_BREAK = registerNode("effect", "break", new NodeData.NodeDataMaker("Break", "Breaks a block", BreakEffectNode::new), Map.of(
+    ));
+    public static final NodeData EFFECT_FIRE = registerNode("effect", "fire", new NodeData.NodeDataMaker("Fire", "Set on fire", FireEffectNode::new), Map.of(
+    ));
     public static final NodeData EFFECT_PROVIDER = registerNode("effect", "effect_provider", new NodeData.NodeDataMaker("Effect Provider", "List of effects", EffectProviderNode::new), Map.of(
             "effect", new SubNodeData("Effect","A single effect", EffectNode.class, List.of(
                     Nodes.EFFECT_FIRE.identifier(),
                     Nodes.EFFECT_BREAK.identifier()
             ))
     ));
-    public static final NodeData EFFECT_BREAK = registerNode("effect", "break", new NodeData.NodeDataMaker("Break", "Breaks a block", BreakEffectNode::new), Map.of(
-    ));
-    public static final NodeData EFFECT_FIRE = registerNode("effect", "fire", new NodeData.NodeDataMaker("Fire", "Set on fire", FireEffectNode::new), Map.of(
-    ));
-    public static final NodeData INSTRUCTION_PROVIDER = registerNode("instruction", "instruction_provider", new NodeData.NodeDataMaker("Instruction Provider", "List of instructions", InstructionProviderNode::new), Map.of(
-            "instruction", new SubNodeData("Instruction","A single instruction", InstructionNode.class, List.of(
-                    Nodes.INSTRUCTION_GENERATE_SHAPE.identifier(),
-                    Nodes.INSTRUCTION_MODIFY_POSITION.identifier()
-            ))
-    ));
-    public static final NodeData INSTRUCTION_GENERATE_SHAPE = registerNode("instruction", "generate_shape", new NodeData.NodeDataMaker("Generate Shape", "Generates a shape", GenerateShapeInstructionNode::new), Map.of(
-            "shape", new SubNodeData("Shape","The shape to generate", ShapeNode.class, List.of(
-                    Nodes.SHAPE_PROJECTILE.identifier(),
-                    Nodes.SHAPE_RAY.identifier()
-            ))
-    ));
-    public static final NodeData INSTRUCTION_MODIFY_POSITION = registerNode("instruction", "modify_position", new NodeData.NodeDataMaker("Modify Position", "Changes the position context", ModifyPositionInstructionNode::new), Map.of(
-    ));
+    
     public static final NodeData SHAPE_PROJECTILE = registerNode("shape", "projectile", new NodeData.NodeDataMaker("Projectile", "Projectile like spell", ProjectileShapeNode::new), Map.of(
             "effects", new SubNodeData("Effects","The effects to apply", EffectProviderNode.class, List.of(
                     Nodes.EFFECT_PROVIDER.identifier()
@@ -55,6 +42,21 @@ public class Nodes {
     public static final NodeData SHAPE_RAY = registerNode("shape", "ray", new NodeData.NodeDataMaker("Ray", "Ray like spell", RayShapeNode::new), Map.of(
             "effects", new SubNodeData("Effects","The effects to apply", EffectProviderNode.class, List.of(
                     Nodes.EFFECT_PROVIDER.identifier()
+            ))
+    ));
+    public static final NodeData INSTRUCTION_GENERATE_SHAPE = registerNode("instruction", "generate_shape", new NodeData.NodeDataMaker("Generate Shape", "Generates a shape", GenerateShapeInstructionNode::new), Map.of(
+            "shape", new SubNodeData("Shape","The shape to generate", ShapeNode.class, List.of(
+                    Nodes.SHAPE_PROJECTILE.identifier(),
+                    Nodes.SHAPE_RAY.identifier()
+            ))
+    ));
+    
+    public static final NodeData INSTRUCTION_MODIFY_POSITION = registerNode("instruction", "modify_position", new NodeData.NodeDataMaker("Modify Position", "Changes the position context", ModifyPositionInstructionNode::new), Map.of(
+    ));
+    public static final NodeData INSTRUCTION_PROVIDER = registerNode("instruction", "instruction_provider", new NodeData.NodeDataMaker("Instruction Provider", "List of instructions", InstructionProviderNode::new), Map.of(
+            "instruction", new SubNodeData("Instruction","A single instruction", InstructionNode.class, List.of(
+                    Nodes.INSTRUCTION_GENERATE_SHAPE.identifier(),
+                    Nodes.INSTRUCTION_MODIFY_POSITION.identifier()
             ))
     ));
 
