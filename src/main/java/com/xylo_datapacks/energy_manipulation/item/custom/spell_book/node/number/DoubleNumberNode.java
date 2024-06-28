80455ce4d8ffef108dddc0afc97020be37be21f4
XyloDatapacks <xylo.datapacks@gmail.com>
2024-06-28 14:19:04 +0200

    removed abstract node with value

--- src/main/java/com/xylo_datapacks/energy_manipulation/item/custom/spell_book/node/number/DoubleNumberNode.java
+++ src/main/java/com/xylo_datapacks/energy_manipulation/item/custom/spell_book/node/number/DoubleNumberNode.java
@@ -1,17 +1,19 @@
 package com.xylo_datapacks.energy_manipulation.item.custom.spell_book.node.number;
 
 import com.xylo_datapacks.energy_manipulation.item.custom.spell_book.node.Nodes;
-import com.xylo_datapacks.energy_manipulation.item.custom.spell_book.node.base_class.AbstractNodeWithValue;
+import com.xylo_datapacks.energy_manipulation.item.custom.spell_book.node.base_class.AbstractNodeWithMap;
 import com.xylo_datapacks.energy_manipulation.item.custom.spell_book.node.base_class.SubNode;
 import com.xylo_datapacks.energy_manipulation.item.custom.spell_book.node.value_type.DoubleValueTypeNode;
 
 import java.util.List;
 
-public class DoubleNumberNode extends AbstractNodeWithValue<DoubleValueTypeNode> implements NumberNode {
+public class DoubleNumberNode extends AbstractNodeWithMap implements NumberNode {
+    SubNode<DoubleValueTypeNode> value = registerSubNode("value", new SubNode.Builder<DoubleValueTypeNode>()
+            .addNodeValues(List.of(
+                    Nodes.VALUE_TYPE_DOUBLE))
+            .build(this));
     
     public DoubleNumberNode() {
-        super(Nodes.NUMBER_DOUBLE, "value", new SubNode.Builder<DoubleValueTypeNode>()
-                .addNodeValues(List.of(Nodes.VALUE_TYPE_DOUBLE))
-        );
+        super(Nodes.NUMBER_DOUBLE);
     }
 }
